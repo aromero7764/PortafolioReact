a52ba5be2618d1a8fc31311458cb6964397e22ab
Alberto Romero <52888483+aromero7764@users.noreply.github.com>
2024-09-27 20:52:42 -0400

    Update index.js
    disable

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit'
-import lenguajesSlice from './slices/lenguajes.slice'
+import { configureStore } from '@reduxjs/toolkit';
+import lenguajesSlice from './slices/lenguajes.slice';
 
 export default configureStore({
   reducer: {
     lenguajes: lenguajesSlice,
-
-	}
-})
+  },
+  devTools: process.env.NODE_ENV === 'development', // DevTools solo habilitado en desarrollo
+});
